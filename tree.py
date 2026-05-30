64fcbed657839e0ded9932bbfa9ab03d7d83dd1a
wpietak <wojciech.pietak5@gmail.com>
2026-05-30 19:17:41 +0200

    create tree

--- tree.py
+++ tree.py
@@ -1,19 +1,31 @@
 import os
 
+tree_file = "tree.out"
+
 ignore = {".git", "__pycache__", ".ipynb_checkpoints"}
 
-def print_tree(path=".", prefix=""):
-    items = sorted(os.listdir(path))
-    items = [i for i in items if i not in ignore]
+tree_out = ""
 
-    for i, item in enumerate(items):
-        full_path = os.path.join(path, item)
-        connector = "└── " if i == len(items) - 1 else "├── "
+with open(tree_file, "w", encoding="utf-8") as f:
 
-        print(prefix + connector + item)
+    def print_tree(path=".", prefix=""):
 
-        if os.path.isdir(full_path):
-            extension = "    " if i == len(items) - 1 else "│   "
-            print_tree(full_path, prefix + extension)
+        items = sorted(os.listdir(path))
+        items = [i for i in items if i not in ignore]
+        
+        for i, item in enumerate(items):
+            full_path = os.path.join(path, item)
+            connector = "└── " if i == len(items) - 1 else "├── "
 
-print_tree()
+            globals()["tree_out"] = globals()["tree_out"] + prefix + connector + item + "\n"
+            #print(prefix + connector + item)
+
+            if os.path.isdir(full_path):
+                extension = "    " if i == len(items) - 1 else "│   "
+                print_tree(full_path, prefix + extension)
+    
+    print_tree()
+    
+    f.write(globals()["tree_out"])
+
+print(tree_out)
